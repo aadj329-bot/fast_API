ddd56f0bd127e3709dbf27fa5031ad3495afd482
Betterthanyesterday <aadj329@gmail.com>
2026-09-17 17:44:07 -0400

    updated with multiple test with some corrections

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,20 @@
 from fastapi.testclient import TestClient
-
 from app.main import app
 
 client = TestClient(app)
 
-def test_read_root():
+def test_profile_endpoint():
     response = client.get("/profile")
+    data = response.json()
+
+    assert response.status_code == 200
+    assert data["username"] == "Aaron"
+    assert data["full_name"] == "Aaron D Johnson"
+    assert data["primary_language"] == "Python"
+    assert data["secondary_language"] == "SQL" 
+
+def test_skills_endpoint():
+    response = client.get("/skills")
     data = response.json()
 
     assert response.status_code == 200
@@ -13,3 +22,19 @@ def test_read_root():
     assert "FastAPI" in data["frameworks"]
     assert "Pytest" in data["frameworks"]
     assert "GitHub" in data["tools"]
+    assert "CI/CD" in data["concepts"]
+
+def test_status_endpoints():
+    response = client.get("/status")
+    data = response.json()
+
+    assert response.status_code == 200
+    assert data["motivation_level"] == "High" 
+    assert data["last_updated"] == "2026-09-15"
+    assert len(data["goals"]) >= 3
+
+def test_invalid_route_404():
+    response = client.get("/defunk_route")
+    assert response.status_code == 404    
+
+    
